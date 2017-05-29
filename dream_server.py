6a9a4e614fdceab30557921716159ee3636cd2b9
Chandler Watson <use.an.alias@gmail.com>
2017-05-28 21:35:05 -0700

    Remove all that extra stuff (v. specific)

--- dream_server.py
+++ dream_server.py
@@ -1,27 +1,30 @@
 import boto3
 import caffe
+import shutil
 
 S3_PULL_KEY = 'dream_up.jpg'
 S3_PUSH_KEY = 'dream_down.jpg'
+TRAIN_VAL_FILE = 'train_val.prototxt'
+DEPLOY_FILE = 'deploy.prototxt'
 SOLVER_FILE = 'solver.prototxt'
-OUTPUT_FILE = '' 
+OUTPUT_FILE = 'output.jpg' 
 
 s3 = boto3.resource('s3')
 
 
 def run(args):
-    run_networks(args['work-dir']) 
     if args['run']:
         bucket = s3.Bucket(args['bucket-name'])
-        pull_frame(bucket)
+        pull_frame(bucket, args['work-dir'])
+        run_networks(args['work-dir']) 
         push_frame(bucket)
     elif args['train']:
         copy_network_data()
         fine_tune()
 
 
-def pull_frame(bucket):
-    bucket.download_file(S3_PULL_KEY, WORK_DIR)
+def pull_frame(bucket, work_dir):
+    bucket.download_file(S3_PULL_KEY, work_dir)
 
 
 def run_networks(work_dir):
@@ -30,10 +33,6 @@ def run_networks(work_dir):
 
 def push_frame(bucket):
     bucket.upload_file(OUTPUT_FILE, S3_PUSH_KEY)
-
-
-def copy_network_data()
-    pass
 
 
 def fine_tune():
