5af3595427a9f557bdbff92cad41bab548853453
Chandler Watson <watson@facni.com>
2017-05-30 05:29:25 +0000

    Add runtime loop

--- dream_server.py
+++ dream_server.py
@@ -1,6 +1,8 @@
 import os.path
+import atexit
 import shutil
 import tempfile
+import time
 
 import batcountry
 import boto3
@@ -14,23 +16,29 @@ S3_PUSH_KEY = 'dream_down.jpg'
 
 def run(args):
     bc = batcountry.BatCountry(args['network'])
+    atexit.register(lambda: bc.cleanup())
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(args['bucket-name'])
-    pull_image_path = pull_frame(bucket)
-    push_image_path = run_network(pull_image_path, bucket, bc) 
-    push_frame(push_image_path, bucket)
+    while True:
+	objects = list(bucket.objects.filter(Prefix=S3_PULL_KEY))
+	if len(objects) == 0 or objects[0].key != S3_PULL_KEY:
+            time.sleep(5)
+	    continue
+        pull_image_path = pull_frame(bucket)
+        push_image_path = run_network(pull_image_path, bucket, bc) 
+        push_frame(push_image_path, bucket)
 
 
 def pull_frame(bucket):
     pull_image = tempfile.NamedTemporaryFile(delete=False, suffix='.jpg')
     bucket.download_file(S3_PULL_KEY, pull_image.name)
+    bucket.delete_objects(Delete={'Objects': [{'Key': S3_PULL_KEY}]})
     return pull_image.name
 
 
 def run_network(pull_image_path, bucket, bc):
     pull_image = Image.open(pull_image_path)
     push_image_array = bc.dream(np.float32(pull_image))
-    bc.cleanup()
     push_image = tempfile.NamedTemporaryFile(delete=False, suffix='.jpg')
     result = Image.fromarray(np.uint8(push_image_array))
     result.save(push_image.name)
